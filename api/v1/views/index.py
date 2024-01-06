4b8987851dbf3b7db6326eb667652af35a84828e
siphoPrince <mabirimisesipho@gmail.com>
2024-01-06 04:03:28 -0800

    index stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,18 @@ def status():
 
             "status": "OK"
     }
+
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def stats():
+    """Retrieves the number of each object by type"""
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+    stats_dict = {}
+
+    for cls_name in classes:
+        cls = storage.classes.get(cls_name)
+        if cls:
+            count = storage.count(cls)
+            stats_dict[cls_name.lower()] = count
+
+    return jsonify(stats_dict)
