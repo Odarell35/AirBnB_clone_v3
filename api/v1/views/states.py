5ea4cd0531b93a5a9d39a0e795da5e7dfb86ba9e
Odarell35 <leratomawasha0@gmail.com>
2024-01-06 12:50:10 -0800

    get state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,53 +2,41 @@
 """
 List all states
 """
-from flask import jsonify, request
-from flask.views import MethodView
-from models import State  # Make sure to import your State model
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.state import State
+from models import storage
+
+@app_views.route("/states", strict_slashes=False, methods=["GET"] )
+def get_all_state():
+        """
+        get all instance
+        """
+        states_list = []
+        states = storage.all(State).values()
+        for state_obj in states:
+            states_list.append(state_obj.to_dict())
+        return jsonify(states_list)
 
 
-class StatesAPI(MethodView):
-    def get(self, state_id=None):
-        """
-        get instance
-        """
-        if state_id is None:
-            states = State.query.all()
-            states_list = [state.to_dict() for state in states]
-            return jsonify(states_list)
-        else:
-            state = State.query.get(state_id)
-            if state:
-                return jsonify(state.to_dict())
-            else:
-                return jsonify({"error": "State not found"}), 404
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET"] )
+def get_state(state_id):
+    """get specific state"""
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        abort(404)
 
-    def post(self):
-        """
-        post state
-        """
-        data = request.get_json()
-        new_state = State(**data)
-        new_state.save()  # Assuming there is a save method in your State model
-        return jsonify(new_state.to_dict()), 201
 
-    def put(self, state_id):
-        """
-        update a specific state by ID
-        """
-        state = State.query.get(state_id)
-        if state:
-            data = request.get_json()
-            state.update(data)  # Assuming there is an update method in your State model
-            return jsonify(state.to_dict())
-        else:
-            return jsonify({"error": "State not found"}), 404
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["DELETE"] )
+def del_state(state_id):
+    """DELETE STATE"""
+    state = storage.get(State, state_id)
+    if state:
+            storage.delete(state)
+            storage.save()
+            return jsonify({}), 200
+    else:
+        abort(404)
 
-    def delete(self, state_id):
-        '''Delete a specific state by ID'''
-        state = State.query.get(state_id)
-        if state:
-            state.delete()  # Assuming there is a delete method in your State model
-            return jsonify({"message": "State deleted successfully"})
-        else:
-            return jsonify({"error": "State not found"}), 404
