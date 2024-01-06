fa67cd63981cc56377f20df2c78bf7cbb057ae37
siphoPrince <mabirimisesipho@gmail.com>
2024-01-06 05:07:38 -0800

    jsonified state

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module"""
 from api.v1.views import app_views
-
+from flask import jsonify
+from models import storage
 
 @app_views.route('/status')
 def status():
@@ -12,14 +13,18 @@ def status():
     }
 
 
-@app_views.route('/stats', strict_slashes=False)
-def count():
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
     """
-    Retrieves the number of each objects by type
+    Retrieves the number of each object type.
     """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+
+    return jsonify(stats)
