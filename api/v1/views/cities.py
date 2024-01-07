d26ebf671af2255731d25252e5d387f4a6452888
Odarell35 <leratomawasha0@gmail.com>
2024-01-06 23:15:34 -0800

    city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -34,6 +34,49 @@ def get_city_obj(city_id):
     if obj:
         return jsonify(obj.to_dict())
     else:
-        return {
-                "ERROR"
-                }
+        abort(404)
+
+
+@app_views.route("/states/<state_id>/cities", strict_slashes=False, methods=["POST"])
+def creates_city(state_id):
+    """creates city with given state_id"""
+    json_data = request.get_json()
+    if not json_data:
+        abort(400, "Not a JSON")
+    if "name" not in json_data:
+        abort(400, "Missing name")
+    state = storage.get(State, state_id)
+    if state:
+        json_data['state_id'] = state_id
+        new_city = City(**json_data)
+        new_city.save()
+        return jsonify(new_city.to_dict()), 201
+    else:
+        abort(404)
+
+
+@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["PUT"])
+def updates_city(city_id):
+    """updates"""
+    json_data = request.get_json()
+    if not json_data:
+        abort(400, "Not a JSON")
+    prev_city = storage.get(City, city_id)
+    if prev_city:
+        prev_city.name = json_data.get("name", prev_city.name)
+        prev_city.save()
+        return jsonify(prev_city.to_dict()), 200
+    else:
+        abort (404)
+
+
+@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["DELETE"])
+def del_city(city_id):
+    """deletes """
+    city = storage.get(City, city_id)
+    if city:
+            storage.delete(city)
+            storage.save()
+            return jsonify({}), 200
+    else:
+        abort(404)
